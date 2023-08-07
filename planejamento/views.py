9ad616f5306c875078fe921f1eccebe15ca4f4cc
EdioHub <ediomarcosgit@gmail.com>
2023-08-06 23:23:28 -0300

    versão 3.0

--- planejamento/views.py
+++ planejamento/views.py
@@ -21,3 +21,7 @@ def update_valor_categoria(request, id):
     categoria.save()
     #return JsonResponse({'valor': f'recebi o valor{id}'})
     return JsonResponse({'status' : 'sucesso'})
+
+def ver_planejamento(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'ver_planejamento.html', {'categorias' : categorias})
